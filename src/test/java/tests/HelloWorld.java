9afe477416f2a834ce1044d72ba4a5cc5319fe01
David Sasson <davidsasson@Davids-MacBook-Pro.local>
2017-10-23 16:28:58 -0700

    made changes

--- src/test/java/tests/HelloWorld.java
+++ src/test/java/tests/HelloWorld.java
@@ -3,6 +3,7 @@ package tests;
 import com.applitools.eyes.BatchInfo;
 import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.selenium.Eyes;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +14,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
  */
 public class HelloWorld
 {
-    public static void main( String[] args ) throws Exception {
+    @Test
+    public void helloTest() throws Exception {
 
         // Open a Chrome browser.
         WebDriver driver = new ChromeDriver();
